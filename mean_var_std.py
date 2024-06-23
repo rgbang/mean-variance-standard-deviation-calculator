63c9c776256f6f6bc9896a53f7ffa2ef5509edc7
rgbang <73834756+rgbang@users.noreply.github.com>
2024-06-23 04:08:43 +0200

    mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -6,29 +6,20 @@ def calculate(lists):
         array = array_t.reshape(3, 3)
         #print(array)
         calculations = {
-            'mean': [np.mean(array, axis=0).tolist(), 
-                     np.mean(array, axis=1).tolist(),
-                     np.mean(array)],
-          
-            'variance': [np.var(array, axis=0).tolist(), 
-                         np.var(array, axis=1).tolist(),
-                         np.var(array.flatten())],
-          
-            'standard deviation': [np.std(array, axis=0).tolist(), 
-                                   np.std(array, axis=1).tolist(),
-                                   np.std(array.flatten())],       
-            'max': [np.max(array, axis=0).tolist(), 
-                    np.max(array, axis=1).tolist(), 
-                    np.max(array.flatten())],
-          
-            'min': [np.min(array, axis=0).tolist(), 
-                    np.min(array, axis=1).tolist(), 
-                    np.min(array.flatten())],
-          
-            'sum': [np.sum(array, axis=0).tolist(), 
-                    np.sum(array, axis=1).tolist(), 
-                    np.sum(array.flatten())]
+      
+            'mean': [np.mean(array, axis=0).tolist(), np.mean(array, axis=1).tolist(), np.mean(array)],       
+                                      
+            'variance': [np.var(array, axis=0).tolist(), np.var(array, axis=1).tolist(),  np.var(array.flatten())],      
+                                        
+            'standard deviation': [np.std(array, axis=0).tolist(),   np.std(array, axis=1).tolist(),np.std(array.flatten())],     
+                                               
+            'max': [np.max(array, axis=0).tolist(), np.max(array, axis=1).tolist(),  np.max(array.flatten())],     
+                                   
+            'min': [np.min(array, axis=0).tolist(), np.min(array, axis=1).tolist(),   np.min(array.flatten())],     
+                                 
+            'sum': [np.sum(array, axis=0).tolist(), np.sum(array, axis=1).tolist(), np.sum(array.flatten())]                         
             }
     else:
+        
         raise ValueError('List must contain nine numbers.')
     return calculations
